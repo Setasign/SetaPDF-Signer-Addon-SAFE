15d88023409c4d98ebd7d0c7a4e7546fd5a01338
Jan Slabon <jan.slabon@setasign.com>
2022-11-14 16:17:51 +0100

    Updated $httpClient implementation + Check for settings.php file

--- examples/list-credentials.php
+++ examples/list-credentials.php
@@ -9,11 +9,15 @@ error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
 
+if (!file_exists('settings.php')) {
+    echo 'The settings.php file is missing. See settings.php.dist for an example.';
+    die();
+}
+
 $settings = require 'settings.php';
 $apiUri = $settings['apiUri'];
 
 $httpClient = new GuzzleHttp\Client();
-$httpClient = new Mjelamanov\GuzzlePsr18\Client($httpClient);
 $requestFactory = new Http\Factory\Guzzle\RequestFactory();
 $streamFactory = new Http\Factory\Guzzle\StreamFactory();
 $client = new Client(
